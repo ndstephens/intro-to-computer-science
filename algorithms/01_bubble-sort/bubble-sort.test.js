684e4f559492a9666819e094457719bd10f087e0
Nate Stephens <nate.dev@fivefloral.com>
2022-07-08 21:34:00 -0400

    solve bubble sort

--- algorithms/01_bubble-sort/bubble-sort.test.js
+++ algorithms/01_bubble-sort/bubble-sort.test.js
@@ -2,9 +2,9 @@
   Write a bubble sort here
   Name the function bubbleSort
   Return the sorted array at the end
-  
+
   To run the tests, change the `test.skip(…)` below to `test(…)`
-  
+
   Bubble sort works by comparing two adjacent numbers next to each other and then
   swapping their places if the smaller index's value is larger than the larger
   index's. Continue looping through until all values are in ascending order
@@ -12,11 +12,28 @@
 
 function bubbleSort(nums) {
   // code goes here
+  let didSwap = false;
+  let length = nums.length;
+
+  do {
+    didSwap = false;
+    for (let i = 0; i < length - 1; i++) {
+      const [a, b] = [nums[i], nums[i + 1]];
+      if (a > b) {
+        nums[i] = b;
+        nums[i + 1] = a;
+        didSwap = true;
+      }
+    }
+    length--;
+  } while (didSwap);
+
+  return nums;
 }
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
+test('bubble sort', function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const sortedNums = bubbleSort(nums);
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
